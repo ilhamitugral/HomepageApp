26ceaddcdda05e1e6f032186b30ae28f78bf42ae
İlhami Tuğral <ilhamitugral@gmail.com>
2019-12-11 21:40:20 +0300

    Eklentiler Hızlandırıldı.
    Bootstrap, Jquery ve Fontawesome eklentileri CDN olarak eklendi.

--- inc/header.php
+++ inc/header.php
@@ -11,12 +11,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo @SITE_TITLE; ?></title>
-    <script src="<?php echo $conf["site_url"]; ?>/plugins/jquery/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <!-- CSS Files -->
     <link rel="stylesheet" href="<?php echo $conf["site_url"]; ?>/css/style.css"/>
     <link rel="stylesheet" href="<?php echo $conf["site_url"]; ?>/themes/<?php echo $conf["theme"]; ?>/css/style.css"/>
-    <link rel="stylesheet" href="<?php echo $conf["site_url"]; ?>/plugins/fontawesome/css/all.css">
-    <link rel="stylesheet" href="<?php echo $conf["site_url"]; ?>/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
     <span class="site-name d-none"><?php echo $conf["site_url"]; ?></span>
