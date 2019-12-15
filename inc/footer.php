f89b2faceb58e4e78b2a239c65be98fe9f849d39
İlhami Tuğral <ilhamitugral@gmail.com>
2019-12-15 12:14:38 +0300

    CDN düzenlemesi yapıldı
    CDN düzenlemesi yapıldı. JS dosyaları sorunsuz giderildi.

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,8 @@
 </body>
-<script src="<?php echo $conf["site_url"]; ?>/plugins/jquery/jquery-ui.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script src="<?php echo $conf["site_url"]; ?>/js/main.js"></script>
-<script src="<?php echo $conf["site_url"]; ?>/plugins/popper/popper.min.js"></script>
-<script src="<?php echo $conf["site_url"]; ?>/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/esm/popper.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script>
     $(function () {
         $('[data-toggle="tooltip"]').tooltip();
