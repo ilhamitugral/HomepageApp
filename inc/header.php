b3d11ffc76e860a36f444f2d58339cf873934366
İlhami Tuğral <ilhamitugral@gmail.com>
2019-12-15 13:45:07 +0300

    FontAwesome CDN güncellendi
    5.11 sürümüne yükseltildi.

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
     <!-- CSS Files -->
     <link rel="stylesheet" href="<?php echo $conf["site_url"]; ?>/css/style.css"/>
     <link rel="stylesheet" href="<?php echo $conf["site_url"]; ?>/themes/<?php echo $conf["theme"]; ?>/css/style.css"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
